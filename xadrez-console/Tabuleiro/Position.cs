0350d0a85f3db94d882910dffaa6d51d1555f9d8
Esdra <69021404+esdrasantos@users.noreply.github.com>
2021-10-30 23:13:17 -0300

    Added Position.cs

---
+++ xadrez-console/Tabuleiro/Position.cs
@@ -0,0 +1,19 @@
+﻿namespace Tabuleiro
+{
+    public class Position
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Position(int row, int column)
+        {
+            this.Row = row;
+            this.Column = column;
+        }
+
+        public override string ToString()
+        {
+            return this.Row + "," + this.Column;
+        }
+    }
+}
