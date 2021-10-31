045d045363bef5111a180f76d293f289c9710419
Esdra <69021404+esdrasantos@users.noreply.github.com>
2021-10-30 23:33:49 -0300

    Started to implement the board and piece classes.

---
+++ xadrez-console/Tabuleiro/Board.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuleiro
+{
+    public class Board
+    {
+        public int NumberOfRows { get; set; }
+
+        public int NumberOfColumns { get; set; }
+
+        public Piece[,] pieces { get; set; }
+
+        public Board(int numberOfRows, int numberOfColumns)
+        {
+            this.NumberOfRows = numberOfRows;
+            this.NumberOfColumns = NumberOfColumns;
+            this.pieces = new Piece[numberOfRows, NumberOfColumns];
+        }
+    }
+}
---
+++ xadrez-console/Tabuleiro/Piece.cs
@@ -0,0 +1,24 @@
+﻿namespace Tabuleiro
+{
+    public class Piece
+    {
+        public Position Position { get; set; }
+
+        public Color Color { get; protected set; }
+
+        public int MovesCount { get; protected set; }
+
+        public Board Board { get; protected set; }
+
+        public const int MovesCountDefault = 0;
+
+        public Piece (Position pos, Color color, Board board)
+        {
+            this.Position = pos;
+            this.Color = Color;
+            this.Board = board;
+
+            this.MovesCount = MovesCountDefault;
+        }
+    }
+}
